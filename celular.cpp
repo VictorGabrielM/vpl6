fa9c38cceedabc0675b31348399bcd115b3b8c24
Victor Gabriel <victorgabrielmoura92@gmail.com>
2021-12-09 16:39:09 -0300

    first commit

---
+++ celular.cpp
@@ -0,0 +1,23 @@
+#include "celular.hpp"
+
+// TODO: Inicialize aqui sua variavel estatica. Caso tenha optado por essa
+// solucao.
+
+Celular::Celular(std::string modelo,
+                 std::string fabricante,
+                 int armazenamento,
+                 int memoria,
+                 double peso,
+                 std::string cor,
+                 int qtd,
+                 float valor) {
+  // TODO: Implemente este metodo
+}
+
+bool Celular::operator<(const Celular& other) {
+  // TODO: Implemente este metodo. 
+  // Preste atencao no exemplo que a descricao utilizada na ordenacao considera
+  // a fabricante antes do modelo do celular
+  
+  return false;
+}
